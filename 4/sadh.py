d7deae8a3cf9ea7affed006743268766b980b99f
Ilya Nazaruk <nazaruchok_1995@mail.ru>
2019-03-25 21:50:05 +0300

    deleted files

--- 4/sadh.py
+++
@@ -1,9 +0,0 @@
-l = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
-key = 0
-while key != len(l):
-    c = str(len((l)))
-    list_keys[key] = list_keys[key] + c
-
-    key += 1
-
-print(l)
